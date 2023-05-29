74735264bad2efcc4804a889c131337e79f2d47c
Yoseph Tamirat <yosephtamir@gmail.com>
2023-05-29 20:38:46 +0300

    Bigger

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         print()
         return int(x)
-    except:
+    except Exception:
         pass
