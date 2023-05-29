5003bb42ffd6aebb13d614ab193e813b77b4623b
Yoseph Tamirat <yosephtamir@gmail.com>
2023-05-29 20:35:47 +0300

    Bigger

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    k = 0
+    for j in my_list:
+        k += 1
+    if k < x:
+        x = k
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+        print()
+        return int(x)
+    except:
+        pass
