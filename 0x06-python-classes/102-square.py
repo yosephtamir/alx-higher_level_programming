ea3cd6ad17f35f97ab23f290707e9691273bfaac
Yoseph Tamirat <yosephtamir@gmail.com>
2023-05-31 22:06:32 +0300

    great

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This is only used to define size
+"""
+
+
+class Square:
+    """
+    the same here
+    """
+    def __init__(self, size=0):
+        """
+        this is an init function
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Used to return the value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        This is used to set the value of the size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        this method is used to return the area of the square
+        """
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
