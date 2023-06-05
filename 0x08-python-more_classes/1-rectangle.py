02aaab24c8ce9b4a1a2dd4edd08f1ca26b418718
Yoseph Tamirat <yosephtamir@gmail.com>
2023-06-05 21:27:19 +0300

    the REC

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,15 +9,24 @@ class Rectangle:
     this is used to calculate parameters of the rectangle
     """
     def __init__(self, width=0, height=0):
+        """
+        this is the instantation of the class
+        """
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
+        """
+        this is used to return the value of width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        This is used to set the value of width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,10 +35,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        This is used to return the value of height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        this is used to set the value of height
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
