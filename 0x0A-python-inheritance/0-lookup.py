87d33092a9da766ef0f58856b32cbd23c6e6fea0
Yoseph Tamirat <yosephtamir@gmail.com>
2023-06-12 18:36:29 +0300

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module is used to return the list of available attributes and methods
+"""
+
+
+def lookup(obj):
+    """
+    This module returns the list of attributes
+    """
+
+    return dir(obj)
