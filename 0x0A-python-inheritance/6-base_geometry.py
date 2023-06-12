71a9cbe8088aac06ace11ef40fca0962e305a33c
Yoseph Tamirat <yosephtamir@gmail.com>
2023-06-12 20:36:38 +0300

    area undefined

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This function modifies the BaseGeometery class
+"""
+
+
+class BaseGeometry:
+    """
+    This is on progress
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
