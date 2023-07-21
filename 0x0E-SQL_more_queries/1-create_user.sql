eec7805fe66a6136db789ed71d066e1e62fbc585
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-21 12:01:10 +0300

    users

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates the MySQL server user user_0d_1
+-- the script should not fail if it exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
