58e8ea0a3ff67fe9181dd98755512685e01749d2
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-21 21:36:16 +0300

    PRIV

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This script show grant permission for the users of localhost
 -- user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
