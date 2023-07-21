a9be3c282c21334bf5f001be76363aa32e596649
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-21 22:34:44 +0300

    flush

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- This script creates the MySQL server user user_0d_1
 -- the script should not fail if it exists
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILAGES;
