1bc1ea3bc5cdb9abe88f897333d8afb1bbe040ca
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-21 22:04:24 +0300

    FORCE NAME

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 --This scipt creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL)
