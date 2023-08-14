9a56bed859f85d776863e0265c3af346bfb04c3e
Yoseph Tamirat <yosephtamir@gmail.com>
2023-08-15 00:01:20 +0300

    semi

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arg = Number(process.argv.length);
+const arg = process.argv.length;
 
 if (arg > 3) {
   console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
