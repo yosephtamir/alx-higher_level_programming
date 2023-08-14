9f00493d838ce44b2a5b5c6ba3dd36bc49857286
Yoseph Tamirat <yosephtamir@gmail.com>
2023-08-15 00:31:54 +0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,12 @@
 const arg = process.argv.length;
 
 if (arg > 3) {
-  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
+  const innerarg = Number(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
+  if (innerarg) {
+    console.log(innerarg);
+  } else {
+    console.log(innerarg);
+  }
 } else {
   console.log(0);
 }
