ae9874788566d878b6ced2da49b64784379634bc
root <yosephtamir@gmail.com>
2023-08-20 03:50:48 -0700

    code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,20 +10,28 @@ if __name__ == '__main__':
     """
     This script only works directly
     """
-    Mydb = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                           passwd=argv[2], db=argv[3])
 
-    Mycur = Mydb.cursor()
-    Mycur.execute("SELECT cities.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            WHERE states.name LIKE BINARY %(state_name)s \
-            ORDER BY cities.id ASC", {'state_name': argv[4]})
-    rows = Mycur.fetchall()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    length = len(rows)
-    for row in range(len(rows)):
-        for colmn in rows[row]:
-            if row < length - 1:
-                print(colmn, end=", ")
-            else:
-                print(colmn)
+    with db.cursor() as Mycur:
+        Mycur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+
+        rows = Mycur.fetchall()
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
