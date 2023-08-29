6190f776bf374a368eec6680fb840cf7994ee3c4
Yoseph Tamirat <yosephtamir@gmail.com>
2023-08-29 09:39:29 +0300

    fie

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,10 +9,5 @@ def write_file(filename="", text=""):
     """
     this is used to write a text to a filename
     """
-    count = 0
     with open(filename, 'w', encoding="utf-8") as e:
-        
-        for i in range(len(text)):
-            count += 1
-        e.write(text)
-    return count
+        return e.write(text)
