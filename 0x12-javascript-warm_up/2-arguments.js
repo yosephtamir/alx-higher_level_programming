b60beab423c145f10fd1f2025e2e6ac36e6ec291
yosephtamir <yosephtamir@gmail.com>
2023-11-13 20:02:24 +0300

    semi

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const length = argv.length;
+
+if (length === 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
