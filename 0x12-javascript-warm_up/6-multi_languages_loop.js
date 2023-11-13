387f6ef63769fee170e237ff896bbfb2737a14ed
yosephtamir <yosephtamir@gmail.com>
2023-11-13 20:52:49 +0300

    loop'

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arrays = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let count = 0;
+while (count < 3) {
+  console.log(arrays[count]);
+  count = count + 1;
+}
