5f5ec2d71005e7f33916923c116906a772b9e172
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-19 18:32:25 +0300

    ab

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This class is used to delate a word containing a"""
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """This script runs"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.name.like('%a%')).delete()
+    session.commit()
