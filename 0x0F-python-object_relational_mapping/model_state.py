72dc2bc758c3442da3aaa3650e449aba0e30338a
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-19 17:59:52 +0300

    updt

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,3 +12,9 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return '{}'.format(self.id)
