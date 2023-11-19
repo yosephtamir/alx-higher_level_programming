e549583945efc27ccf329c436501b4e31e4a4ddd
Yoseph Tamirat <yosephtamir@gmail.com>
2023-11-19 17:52:29 +0300

    set in

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Used to add a state into a database"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+
+if __name__ == "__main__":
+    """Entery point"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Louisiana = State("Louisiana")
+
+    session.add(Louisiana)
+    session.commit()
+
+    print(Louisiana)
