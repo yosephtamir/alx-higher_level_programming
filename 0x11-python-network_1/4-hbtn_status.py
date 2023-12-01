04fc9c6934671fde004771e168b1bc8054ffa568
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-01 05:21:54 -0800

    body

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,14 +2,13 @@
 """
 a Python script that fetches https://alx-intranet.hbtn.io/status
 """
-from urllib.request import Request, urlopen
+import requests
 
 
 if __name__ == "__main__":
     """This script do not run if imported"""
-    url = Request("https://alx-intranet.hbtn.io/status")
-    with urlopen(url) as response:
-        the_page = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page.decode("utf-8")))
+    the_page = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(the_page.text)))
+    print("\t- content: {}".format(the_page.text))
