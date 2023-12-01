f8a8b04e987b332b477c9334ce917d4afeb1bf06
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-01 05:42:58 -0800

    header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+import requests
+
+
+if __name__ == "__main__":
+    """This script do not run if imported"""
+    the_page = requests.get("https://alx-intranet.hbtn.io/")
+
+    print(the_page.headers['X-Request-Id'])
