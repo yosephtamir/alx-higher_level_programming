bd0bfd31e479b391cc35c1a62e693d4b7b1773f6
Yoseph Tamirat <yosephtamir@gmail.com>
2023-12-01 01:15:26 -0800

    hbtn

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3"""
+import urllib.request
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    """The code won't run if imported"""
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(the_page)))
+        print("    - content: {}".format(the_page))
+        print("    - utf-8 content: {}".format(the_page.decode("utf-8")))
